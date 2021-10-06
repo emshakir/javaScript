d894c376e0d105f03dd775023c2e445b9c514e7b
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2021-10-06 22:39:14 +0530

    Added css tooltip

--- Subscritpion/js/index.js
+++ Subscritpion/js/index.js
@@ -1,11 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#dialog-btn').on('click', () => {
         $('#dialog').dialog({
-            title: "Consultant Subscription",
-            width: 1200,
-            height: 400,
+            title: 'Consultant Subscription',
+            width: 900,
+            height: 250,
             modal: true,
-            closeText: "hide"
+            closeText: 'hide',
         });
     });
+
+    var S = '';
+    $('.inputTag').keypress(function (e) {
+        var input = e.key;
+        S += input;
+        var dInput = this.value;
+        console.log(dInput);
+    });
+
+    /*$('.inputTag').keypress(() => {
+        var input = $(this).val();
+        console.log(input);
+        console.log();
+    });*/
+
+    /* $('.inputTag').on('keypress', (e) => {
+        var input = $(this).val();
+        console.log(input);
+        console.log();
+    });*/
 });
